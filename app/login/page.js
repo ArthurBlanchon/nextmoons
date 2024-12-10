4ff865a00284d9e6f06457da2205e8e81d114963
ArthurOnTime <112695921+ArthurOnTime@users.noreply.github.com>
2024-12-11 00:21:03 +0100

    Revert "added Privacy policies et ToS"
    This reverts commit 951afa4a14a2af60b00e751527358e1917fdd4e3.

--- app/login/page.js
+++ app/login/page.js
@@ -1,7 +1,6 @@
 import { signIn, auth } from "@/lib/auth"
 import { redirect } from "next/navigation"
 import Image from "next/image"
-import Link from 'next/link'
  
 export default async function SignIn() {
   const session = await auth()
@@ -123,7 +122,7 @@ export default async function SignIn() {
 
           {/* Footer */}
           <p className="mt-8 text-center text-small text-gray-500">
-            By continuing, you agree to our <Link href="/terms" className="text-primary hover:underline font-medium">Terms of Service</Link> and <Link href="/privacy" className="text-primary hover:underline font-medium">Privacy Policy</Link>
+            By continuing, you agree to our Terms of Service and Privacy Policy
           </p>
         </div>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -68,8 +68,5 @@ module.exports = {
   		}
   	}
   },
-  plugins: [
-    require("tailwindcss-animate"),
-    require("@tailwindcss/typography")
-  ],
+  plugins: [require("tailwindcss-animate")],
 };
